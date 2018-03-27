6ea2ed3b1dc1bb6b5c7adc33e6709347f6d660ee
Sean Doyle <seancdoyle1@gmail.com>
2018-03-27 18:47:47 +0100

    Working Delete Script
    Basic Build and termination

--- removeAll.py
+++ removeAll.py
@@ -1,32 +1,10 @@
-#!/usr/bin/env python
-"""I was trying to programatically remove a Virtual Private Cloud (VPC) in
-AWS and the error message was not helpful:
-
-    botocore.exceptions.ClientError: An error occurred (DependencyViolation)
-    when calling the DeleteVpc operation: The vpc 'vpc-c12029b9' has
-    dependencies and cannot be deleted.
-
-Searching for a quick solution was not fruitful but I was able to glean some
-knowledge from Neil Swinton's gist:
-https://gist.github.com/neilswinton/d37787a8d84387c591ff365594bd26ed
-
-Using that, and some trial and error, I was able to develop this function
-that does all the cleanup necessary.
-
-Word of warning: This will delete the VPC and all instances/resources
-associated with it. As far as I know, this is complete. It's just like
-selecting Delete from the context menu on a VPC in the AWS Console except
-that this also deletes internet gateways that are attached to the VPC.
-"""
-
 import sys
 import boto3
-
-
+ 
 def vpc_cleanup(vpcid):
-    """Remove VPC from AWS
+    """Remove VPC from AWS 
     Set your region/access-key/secret-key from env variables or boto config.
-
+ 
     :param vpcid: id of vpc to delete
     """
     if not vpcid:
@@ -35,62 +13,95 @@ def vpc_cleanup(vpcid):
     ec2 = boto3.resource('ec2')
     ec2client = ec2.meta.client
     vpc = ec2.Vpc(vpcid)
+
+    print('##############################')
+    print('Hold Onto Your Hat...........')
+    print('##############################')
+    # delete any instances
+    for subnet in vpc.subnets.all():
+        print('Termination In Progress')
+        for instance in subnet.instances.all():
+            instance.terminate()
+            print('In Progress......')
+            instance.wait_until_terminated()
+    print('Instances Gone')
+
+
     # detach and delete all gateways associated with the vpc
     for gw in vpc.internet_gateways.all():
         vpc.detach_internet_gateway(InternetGatewayId=gw.id)
         gw.delete()
-    print('Gateways Done')
 
-    # delete all route table associations
-    for rt in vpc.route_tables.all():
-        for rta in rt.associations:
-            if not rta.main:
-                rta.delete()
-    print('Route Table Done')
-    # delete any instances
-    for subnet in vpc.subnets.all():
-        for instance in subnet.instances.all():
-            instance.terminate()
-    print('Subnets Done')
+    rtl = vpc.route_tables.all()
+    count = sum(1 for _ in rtl)
+    while count > 1:
+
+     #delete all route table associations
+        for rt in rtl:
+            for rta in rt.associations:
+                if not rta.main:
+                    print("Deleting route table associations")
+                    rta.delete()
+
+            for r in rt.routes:
+                try:
+                    x = r.delete()
+                    print(x)
+                except:
+                    pass
+            try:
+                rt.delete()
+            except:
+                pass
+                 
+        rtl = vpc.route_tables.all()
+        count = sum(1 for _ in rtl)
+
+    print('Route Tables Gone')
+
     # delete our endpoints
-    for ep in ec2client.describe_vpc_endpoints(
-            Filters=[{
-                'Name': 'vpc-id',
-                'Values': [vpcid]
-            }])['VpcEndpoints']:
+    for ep in ec2client.describe_vpc_endpoints(Filters=[{
+            'Name': 'vpc-id',
+            'Values': [ vpcid ]
+        }])['VpcEndpoints']:
         ec2client.delete_vpc_endpoints(VpcEndpointIds=[ep['VpcEndpointId']])
-        print('Endpoints Done')
+    print('Endpoints Gone')
 
     # delete our security groups
     for sg in vpc.security_groups.all():
         if sg.group_name != 'default':
             sg.delete()
-    print('Security Groups Done')       
+    print('Security Groups Gone')
+
     # delete any vpc peering connections
-    for vpcpeer in ec2client.describe_vpc_peering_connections(
-            Filters=[{
-                'Name': 'requester-vpc-info.vpc-id',
-                'Values': [vpcid]
-            }])['VpcPeeringConnections']:
+    for vpcpeer in ec2client.describe_vpc_peering_connections(Filters=[{
+            'Name': 'requester-vpc-info.vpc-id',
+            'Values': [ vpcid ]
+        }] )['VpcPeeringConnections']:
         ec2.VpcPeeringConnection(vpcpeer['VpcPeeringConnectionId']).delete()
+    print('VPC Connections Gone')
+
     # delete non-default network acls
     for netacl in vpc.network_acls.all():
         if not netacl.is_default:
             netacl.delete()
+
     # delete network interfaces
     for subnet in vpc.subnets.all():
         for interface in subnet.network_interfaces.all():
             interface.delete()
         subnet.delete()
-    print('Network Interfaces Done')
+    print('Network Interfaces Gone')
+
     # finally, delete the vpc
     ec2client.delete_vpc(VpcId=vpcid)
-    print('VPC Done')
-
-
+    print('##############################')
+    print('My Work Here Is Done')
+    print('##############################')
+ 
+ 
 def main(argv=None):
     vpc_cleanup(argv[1])
-
-
+ 
 if __name__ == '__main__':
     main(sys.argv)
